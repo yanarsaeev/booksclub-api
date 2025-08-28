573ce5370d9131e2f8c7fe6eac600966bb3228fe
yanarsaeev <mackardyback@internet.ru>
2025-08-28 11:52:06 +0300

    add user controller

---
+++ src/main/java/com/booksclub/api/controller/UserRestController.java
@@ -0,0 +1,9 @@
+package com.booksclub.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+}
