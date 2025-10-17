e021739873bffef405b217ff51f877ea50a55faa
yanarsaeev <mackardyback@internet.ru>
2025-10-17 14:53:49 +0300

    rename exception

--- src/main/java/com/booksclub/api/exception/ElementNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.booksclub.api.exception;
-
-public class ElementNotFoundException extends RuntimeException {
-    public ElementNotFoundException(String message) {
-        super(message);
-    }
-}
