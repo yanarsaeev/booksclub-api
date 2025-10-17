9e06ff7548e7a5d6473a83fac5ad0d91ec84e1e8
yanarsaeev <mackardyback@internet.ru>
2025-10-17 14:53:00 +0300

    add exception handlers

--- src/main/java/com/booksclub/api/controller/GlobalExceptionHandler.java
+++ src/main/java/com/booksclub/api/controller/GlobalExceptionHandler.java
@@ -1,18 +1,25 @@
 package com.booksclub.api.controller;
 
-import com.booksclub.api.exception.ElementNotFoundException;
-import com.booksclub.api.util.EventErrorResponse;
+import com.booksclub.api.exception.AppError;
+import com.booksclub.api.exception.ElementFoundException;
+import com.booksclub.api.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler
-    public ResponseEntity<EventErrorResponse> catchEventNotFoundException(ElementNotFoundException exception) {
-        return new ResponseEntity<>(new EventErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage()),
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<AppError> catchElementNotFoundException(NotFoundException exception) {
+        return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), exception.getMessage()),
                 HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ElementFoundException.class)
+    public ResponseEntity<AppError> catchElementFoundException(ElementFoundException exception) {
+        return new ResponseEntity<>(new AppError(HttpStatus.FOUND.value(), exception.getMessage()),
+                HttpStatus.FOUND);
     }
 }
