665f8409d4b4ad04d1a96e96e60ccab26180b117
yanarsaeev <mackardyback@internet.ru>
2025-10-17 14:55:18 +0300

    add constructors

--- src/main/java/com/booksclub/api/dto/PersonDto.java
+++ src/main/java/com/booksclub/api/dto/PersonDto.java
@@ -1,10 +1,11 @@
 package com.booksclub.api.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class PersonDto {
     private Long id;
